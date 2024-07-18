9f2d8b24872d55859b6aaccfcf47ad998ef1e02d
anmol <softwaredev@csmaviation.com>
2024-07-18 00:52:38 -0700

    #1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -49,7 +49,7 @@ const Hero = () => {
             <motion.img
               initial={{ x: 100, opacity: 0 }}
               animate={{ x: 0, opacity: 1 }}
-              transition={{ duration: 1, delay: 1.2 }}
+              transition={{ duration: 1, delay: 0.5 }}
               className="rounded-3xl"
               src={profilePic}
               alt="anmol singh"
